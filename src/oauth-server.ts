88caf6590f60f8e65f5f2b90d55e9ab60d2ca4d2
Jordan Burke <jordan.burke@gmail.com>
2025-08-14 17:18:29 -0400

    New Oauth Checks

--- src/oauth-server.ts
+++ src/oauth-server.ts
@@ -48,6 +48,18 @@ setInterval(() => {
 
 export function createOAuthServer(oauth: OAuthConfig, mcpPort: number): Express {
   const app = express()
+
+  // Add CORS middleware for all OAuth endpoints
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200)
+    }
+    next()
+  })
+
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
 
